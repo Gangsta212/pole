732d063d849ad5332db0cc63781fbd22795b21c5
Gangsta212 <nitinteotia213@gmail.com>
2024-12-17 21:43:13 +0530

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -12,13 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import main
 
-
-def test_index() -> str:
-    main.app.testing = True
-    client = main.app.test_client()
-
-    r = client.get("/")
-    assert r.status_code == 200
-    assert "Hello World" in r.data.decode("utf-8")
